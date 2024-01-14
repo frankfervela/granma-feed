3dc6f017cbe0d406f0dd438693f4a50e6aec9f4d
frankfervela <fafv515@gmail.com>
2024-01-13 19:08:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using FeedReader.Clients;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<GranmaRssClient>(sb => new GranmaRssClient("https://www.granma.cu/feed"));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
